7392f9649e8201b9e0ce8e6e2a2f0705957aaf5b
alex wennerberg <alex@alexwennerberg.com>
2024-06-08 20:04:23 -0400

    Improve styling somewhat

--- util.go
+++ util.go
@@ -24,3 +24,67 @@ func logRequest(handler http.Handler) http.Handler {
 		handler.ServeHTTP(w, r)
 	})
 }
+
+
+// Generate a simple avatar based on a hash of your name
+//
+// Derived from Ted Unangst's Honk
+// https://humungus.tedunangst.com/r/honk/v/tip/f/avatar.go
+//
+// Copyright (c) 2019 Alex Wennerberg, Ted Unangst <tedu@tedunangst.com>
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+import (
+	"bytes"
+	"crypto/sha512"
+	"fmt"
+	"image"
+	"image/png"
+	"io/ioutil"
+	"os"
+)
+
+// Modify the four colors used
+// TODO align with css
+var avatarcolors = [4][4]byte{
+	{0, 0, 0, 255},
+	{255, 255, 255, 255},
+	{172, 172, 172, 255},
+	{96, 96, 96, 255},
+}
+
+// x and y dimensions in pixels
+const size = 400
+
+// generate PNG avatar from string
+func genAvatar(name string) []byte {
+	h := sha512.New()
+	h.Write([]byte(name))
+	s := h.Sum(nil)
+	// Mess with these numbers to change the size
+	img := image.NewNRGBA(image.Rect(0, 0, size, size))
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			p := i*img.Stride + j*4
+			xx := i/(size/4)*16 + j/(size/4)
+			x := s[xx]
+			for n := 0; n < 4; n++ {
+				img.Pix[p+n] = avatarcolors[x/64][n]
+			}
+		}
+	}
+	var buf bytes.Buffer
+	png.Encode(&buf, img)
+	return buf.Bytes()
+}
